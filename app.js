9f2b050fbe2c2faf9f093219f748ca0f79157f9f
Gaël Servaud <gael.servaud@gip-retraite.fr>
2015-06-30 09:25:03 +0300

    Fix Typo app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function Sockets(){}
 
 Sockets.prototype.initialize = function(){
    
-	this.essai = new WebSocket("ws://127.0.0.1:4444")
+	this.socket = new WebSocket("ws://127.0.0.1:4444")
    
 };
 
@@ -10,16 +10,18 @@ Sockets.prototype.update = function(streams){
 	this.data = { "x" : streams.sensors.position.x , 
 				  	"y" : streams.sensors.position.y , 
 						"z" : streams.sensors.position.z,
-							"heading" : streams.sensors.yaw , 
-				  				"roll" : streams.sensors.roll , 
-									"pitch" : streams.sensors.pitch,
-										"wheelFR" : streams.wheelInfo[0][10],
-											"wheelSteerR" : streams.wheelInfo[0][4],
-											"wheelSteerL" : streams.wheelInfo[1][4],
-												"wheeFL" : streams.wheelInfo[1][10],
-													"wheeRR" : streams.wheelInfo[2][10],
-														"wheeRL" : streams.wheelInfo[3][10],
-															"currentSteer" : streams.electrics.steering };
-
-this.essai.send(JSON.stringify(this.data));
+							"heading" : streams.sensors.yaw,
+								"direction" : streams.sensors.direction, 
+				  					"roll" : streams.sensors.roll , 
+										"pitch" : streams.sensors.pitch,
+											"wheelFR" : streams.wheelInfo[0][10],
+													"wheeFL" : streams.wheelInfo[1][10],
+														"wheeRR" : streams.wheelInfo[2][10],
+															"wheeRL" : streams.wheelInfo[3][10],
+																"currentSteer" : streams.electrics.steering,
+																	"dx" : streams.sensors.direction.x,
+																		"dy" : streams.sensors.direction.y,
+																			"dz" : streams.sensors.direction.z,
+																				"airspeed" : streams.electrics.airspeed };
+this.socket.send(JSON.stringify(this.data));
 };
